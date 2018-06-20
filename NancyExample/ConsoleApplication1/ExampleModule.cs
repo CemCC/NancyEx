0ff518ba0fae56e7b4b3cc57de8bbd29c47877fe
CemCC <cemcaliskan@ymail.com>
2018-06-20 20:21:49 +1000

    Create example module

---
+++ NancyExample/ConsoleApplication1/ExampleModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace ConsoleApplication1
+{
+    class ExampleModule : NancyModule
+    {
+        public ExampleModule() : base ("/config")
+        {
+            Get["/"] = _ => "Received GET request";
+
+            Post["/important"] = _ => "Received POST request";
+        }
+    }
+}
